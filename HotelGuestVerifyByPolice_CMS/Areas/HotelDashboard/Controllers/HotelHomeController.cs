027036cb5c85ff1606e83d47c7ce3c664f8b53f1
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2023-09-06 12:00:42 +0530

    Comit By Rohit

--- HotelGuestVerifyByPolice_CMS/Areas/HotelDashboard/Controllers/HotelHomeController.cs
+++ HotelGuestVerifyByPolice_CMS/Areas/HotelDashboard/Controllers/HotelHomeController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelGuestVerifyByPolice_CMS.Areas.HotelDashboard.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelGuestVerifyByPolice_CMS.Areas.HotelDashboard.Controllers
 {
+    [Area("HotelDashboard")]
     public class HotelHomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            Location[] salesLocation = new Location[] {
+                new Location
+                {
+                    ID = 1,
+                    Address = "A Road",
+                    City = "Pune"
+                },
+                new Location
+                {
+                    ID = 2,
+                    Address = "B Road",
+                    City = "London"
+                },
+                new Location
+                {
+                    ID = 3,
+                    Address = "K Road",
+                    City = "New York"
+                },
+            };
+            return View(salesLocation);
+            //return View();
         }
     }
 }
---
+++ HotelGuestVerifyByPolice_CMS/Areas/HotelDashboard/Models/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelGuestVerifyByPolice_CMS.Areas.HotelDashboard.Models
+{
+    public class Location
+    {
+        public int ID { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+    }
+}
