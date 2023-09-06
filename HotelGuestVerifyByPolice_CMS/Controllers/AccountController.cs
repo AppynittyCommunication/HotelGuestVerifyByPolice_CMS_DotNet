fcfeaf677dd5965b0a8b6953b0ab003b98e3e5ec
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2023-09-06 11:11:55 +0530

    Comit By Rohit

--- HotelGuestVerifyByPolice_CMS/Controllers/AccountController.cs
+++ HotelGuestVerifyByPolice_CMS/Controllers/AccountController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http.Headers;
 using System.Net.Http;
+using System.Net;
+using HotelGuestVerifyByPolice_CMS.Models.APIModels;
+using Newtonsoft.Json;
 
 namespace HotelGuestVerifyByPolice_CMS.Controllers
 {
@@ -36,9 +39,75 @@ namespace HotelGuestVerifyByPolice_CMS.Controllers
         [HttpPost]
         public async Task<ActionResult> HotelRegistrationAsync(HotelReg model)
         {
-			_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-			HttpResponseMessage rs = await _httpClient.PostAsync(_httpClient.BaseAddress + "SelectList/GetStates", null);
+            //------------Get Ip Start---------------------
 
+            // Getting host name
+            string host = Dns.GetHostName();
+
+            //// Getting ip address using host name
+            IPHostEntry ip = Dns.GetHostEntry(host);
+            string hname = ip.HostName.ToString();
+            string ipAdd = (ip.AddressList[3].ToString());
+            //------------Get Ip End---------------------
+            HotelRegBody hotelRegBody = new();
+
+            hotelRegBody.firstName = model.firstName;
+            hotelRegBody.lastName = model.lastName;
+            hotelRegBody.email = model.email;
+            hotelRegBody.address = model.address;  
+            hotelRegBody.mobile = model.mobile;
+            hotelRegBody.pinCode = model.pinCode;
+            hotelRegBody.hotelName = model.hotelName;
+            hotelRegBody.hotelRegNo = model.hotelRegNo;
+            hotelRegBody.userId = model.userId;
+            hotelRegBody.lat = model.lat;
+            hotelRegBody._long = model._long;
+            hotelRegBody.stateId = model.stateId;
+            hotelRegBody.distId = model.distId;
+            hotelRegBody.cityId = model.cityId;
+            hotelRegBody.stationCode = model.stationCode;
+            if(model.diviceIp == null)
+            {
+                hotelRegBody.diviceIp = ipAdd;
+            }
+            else
+            {
+                hotelRegBody.diviceIp = model.diviceIp;
+            }
+            
+
+
+
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			HttpResponseMessage response = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress + "Account/HotelRegistration", hotelRegBody);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                var dynamicobject = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+                var code = (int)response.StatusCode;
+                var status = dynamicobject.status.ToString();
+                var message = dynamicobject.message.ToString();
+
+                Console.WriteLine("Code" + code);
+                Console.WriteLine("Status" + status);
+                Console.WriteLine("Message" + message);
+            }
+            else
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                var dynamicobject = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+                var code = (int)response.StatusCode;
+                var status = dynamicobject.status.ToString();
+                var message = dynamicobject.message.ToString();
+
+                Console.WriteLine("Code" + code);
+                Console.WriteLine("Status" + status);
+                Console.WriteLine("Message" + message);
+            }
+            
 			return View();
 		}
     }
