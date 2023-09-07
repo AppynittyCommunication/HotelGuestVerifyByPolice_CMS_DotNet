cac82b6ab873c464f644a430e68421c06a51b7a1
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2023-09-07 11:21:52 +0530

    Comit By Rohit

--- HotelGuestVerifyByPolice_CMS/Models/HotelLogin.cs
+++ HotelGuestVerifyByPolice_CMS/Models/HotelLogin.cs
@@ -7,6 +7,7 @@ namespace HotelGuestVerifyByPolice_CMS.Models
         [Required(ErrorMessage = "Hotel Username required.")]
         public string UserName { get; set; }
         [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
