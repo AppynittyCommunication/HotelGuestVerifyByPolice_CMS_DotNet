f876e89cfd97f7e6b558cc49f70eecadb70669ff
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2023-09-08 12:45:40 +0530

    Comit By Rohit

--- HotelGuestVerifyByPolice_CMS/Program.cs
+++ HotelGuestVerifyByPolice_CMS/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddSingleton<IHttpContextAccessor , HttpContextAccessor>();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(option =>
 {
-    option.IdleTimeout = TimeSpan.FromSeconds(10);
+    option.IdleTimeout = TimeSpan.FromSeconds(30);
 });
 
 
