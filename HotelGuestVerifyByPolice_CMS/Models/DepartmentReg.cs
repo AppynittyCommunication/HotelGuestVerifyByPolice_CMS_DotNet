f8dc7d94dc9da4506d92eea69c406b5d9953408c
shubhammanohare88@gmail.com <shubhammanohare88@gamil.com>
2023-09-08 13:37:06 +0530

    Commit By Shubham

--- HotelGuestVerifyByPolice_CMS/Models/DepartmentReg.cs
+++ HotelGuestVerifyByPolice_CMS/Models/DepartmentReg.cs
@@ -8,6 +8,9 @@ namespace HotelGuestVerifyByPolice_CMS.Models
        
         [Required(ErrorMessage = "Username is required.")]
         public string? userId { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        public string? password { get; set; }
+
         [Required(ErrorMessage = "Mobile Number is required.")]
         [RegularExpression("^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number.")]
         public string? mobile { get; set; }
