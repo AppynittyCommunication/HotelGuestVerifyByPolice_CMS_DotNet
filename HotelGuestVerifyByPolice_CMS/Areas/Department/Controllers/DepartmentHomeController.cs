a4a6dfb398e3f55ffc5026412e795247b497efa7
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2023-09-21 13:27:40 +0530

    Comit By Rohit

--- HotelGuestVerifyByPolice_CMS/Areas/Department/Controllers/DepartmentHomeController.cs
+++ HotelGuestVerifyByPolice_CMS/Areas/Department/Controllers/DepartmentHomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelGuestVerifyByPolice_CMS.Areas.Department.Models;
+using HotelGuestVerifyByPolice_CMS.Models.APIModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace HotelGuestVerifyByPolice_CMS.Areas.Department.Controllers
 {
@@ -17,13 +21,79 @@ namespace HotelGuestVerifyByPolice_CMS.Areas.Department.Controllers
             _httpClient.BaseAddress = baseUri;
             _contx = contx;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
             //string hotelregno = _contx.HttpContext.Session.GetString("hotelRegNo");
             string departuser = _contx.HttpContext.Session.GetString("departUser");
+            DeptDashboardRes deptDashboardRes = new ();
+            deptDashboardRes.hotelLocOnDashboards = new();
+            deptDashboardRes.hotelListDetailsForDashboards = new();
+            deptDashboardRes.hotelGuestDetails_DeptDashes = new();
 
-           
-            ViewBag.departuser = departuser;
+
+
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Add("userID", departuser);
+            HttpResponseMessage response = await _httpClient.PostAsync(_httpClient.BaseAddress + "Department/DeptDashboard", null);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                var dynamicobject = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+                deptDashboardRes.code = dynamicobject.code;
+                deptDashboardRes.status = dynamicobject.status;
+                deptDashboardRes.message = dynamicobject.message;
+                deptDashboardRes.stationName = dynamicobject.stationName;
+
+                foreach (var i in dynamicobject.hotelLocOnDashboard)
+                {
+                    deptDashboardRes.hotelLocOnDashboards.Add(new HotelLocOnDashboard
+                    {
+                        hotelName = i.hotelName,
+                        Mobile = i.mobile,
+                        Address = i.address,
+                        lat = i.lat,
+                        _long = i._long,
+                    });
+                    
+                }
+                foreach(var hld in dynamicobject.hotelListDetailsForDashboards) {
+
+                    deptDashboardRes.hotelListDetailsForDashboards.Add(new HotelListDetailsForDashboard
+                    {
+                        stationName = hld.stationName,
+                        hotelCount = hld.hotelCount,
+                        totalCheckIn = hld.totalCheckIn,
+                        todaysCheckIn = hld.todaysCheckIn,
+                        todaysCheckOut = hld.todaysCheckOut,
+
+                    });
+                }
+
+                foreach(var hgd in dynamicobject.hotelGuestDetails_DeptDashes)
+                {
+                    deptDashboardRes.hotelGuestDetails_DeptDashes.Add(new HotelGuestDetails_DeptDash1
+                    {
+                        roomBookingID = hgd.roomBookingID,
+                        guestName = hgd.guestName,
+                        guestPhoto = hgd.guestPhoto,
+                        age = hgd.age,
+                        city = hgd.city,
+                        visitPurpose = hgd.visitPurpose,
+                        comingFrom = hgd.comingFrom,
+                        total_Adult = hgd.total_Adult,
+                        total_Child = hgd.total_Child,
+                        hotelName = hgd.hotelName,
+                        checkInDate = hgd.checkInDate,
+                    });
+                }
+
+
+                ViewBag.departuser = departuser;
+
+                return View(deptDashboardRes);
+            }
 
             if (string.IsNullOrEmpty(departuser))
             {
---
+++ HotelGuestVerifyByPolice_CMS/Areas/Department/Models/DeptDashboardRes.cs
@@ -0,0 +1,49 @@
+﻿namespace HotelGuestVerifyByPolice_CMS.Areas.Department.Models
+{
+    public class DeptDashboardRes
+    {
+        public int code { get; set; }
+        public string? status { get; set; }
+        public string? message { get; set; }
+        public string? stationName { get; set; }
+
+        public List<HotelLocOnDashboard>? hotelLocOnDashboards { get; set; }
+        public List<HotelListDetailsForDashboard>? hotelListDetailsForDashboards { get; set; }
+        public List<HotelGuestDetails_DeptDash1>? hotelGuestDetails_DeptDashes { get; set; }
+    }
+
+    public class HotelLocOnDashboard
+    {
+        public string? hotelName { get; set; }
+        public string? Mobile { get; set; }
+        public string? Address { get; set; }
+        public double? lat { get; set; }
+        public double? _long { get; set; }
+    }
+
+
+    public class HotelListDetailsForDashboard
+    {
+        public string? stationName { get; set; }
+        public int hotelCount { get; set; }
+        public int totalCheckIn { get; set; }
+
+        public int todaysCheckIn { get; set; }
+        public int todaysCheckOut { get; set; }
+    }
+
+    public class HotelGuestDetails_DeptDash1
+    {
+        public string? roomBookingID { get; set; }
+        public byte[]? guestPhoto { get; set; }
+        public string? guestName { get; set; }
+        public int age { get; set; }
+        public string? city { get; set; }
+        public string? visitPurpose { get; set; }
+        public string? comingFrom { get; set; }
+        public int total_Adult { get; set; }
+        public int total_Child { get; set; }
+        public string? hotelName { get; set; }
+        public DateTime checkInDate { get; set; }
+    }
+}
