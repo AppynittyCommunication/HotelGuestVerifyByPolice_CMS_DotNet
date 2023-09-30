89f0097330a55e657dd2616c2f3323bc648d1cbe
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2023-09-30 15:05:22 +0530

    Comit By Rohit

---
+++ HotelGuestVerifyByPolice_CMS/Models/VisitPurpose.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelGuestVerifyByPolice_CMS.Models
+{
+    public class VisitPurpose
+    {
+        public int Code { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public List<VisitPurposeData> Data { get; set; }
+    }
+
+    public class VisitPurposeData
+    {
+        public int id { get; set; }
+        public string? purpose { get; set; }
+    }
+}
