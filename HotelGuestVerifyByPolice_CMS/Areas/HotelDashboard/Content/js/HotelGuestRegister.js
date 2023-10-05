ff0369384b19727d9f95eac318fd8eb2607f4393
umeshl@appynitty.com <umeshl@appynitty.com>
2023-10-05 11:15:36 +0530

    ddddd

---
+++ HotelGuestVerifyByPolice_CMS/Areas/HotelDashboard/Content/js/HotelGuestRegister.js
@@ -0,0 +1,220 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        type: "post",
+        url: "/States/CountryList",
+        datatype: "json",
+        traditional: true,
+        success: function (data) {
+            console.log(data);
+            countrylist = '<option value="">Select Country</option>';
+            for (var i = 0; i < data.length; i++) {
+                countrylist = countrylist + "<option value=" + data[i].countryCode + ">" + data[i].countryName + "</option>";
+            }
+            //district = district + '</select>';
+            $("#countryMain").html(countrylist);
+            $("#countryAdult").html(countrylist);
+        },
+    });
+    $("#countryMain").change(function () {
+        $.ajax({
+            type: "post",
+            url: "/States/CountryWiseStateList",
+            data: { cCode: $("#countryMain").val() },
+            datatype: "json",
+            traditional: true,
+            success: function (data) {
+                // console.log("Selected State Id:" + $('#statelist').val());
+                // console.log(data);
+                var statelist = '<option value="">Select State</option>';
+                for (var i = 0; i < data.length; i++) {
+                    statelist = statelist + "<option value=" + data[i].stateId + ">" + data[i].stateName + "</option>";
+                }
+                $("#stateMain").html(statelist);
+            },
+        });
+    });
+    $("#stateMain").change(function () {
+        $.ajax({
+            type: "post",
+            url: "/States/DistrictList",
+            data: { stateID: $("#stateMain").val() },
+            datatype: "json",
+            traditional: true,
+            success: function (data) {
+                // console.log("Selected State Id:" + $('#statelist').val());
+                // console.log(data);
+                var distlist = '<option value="">Select District</option>';
+                for (var i = 0; i < data.length; i++) {
+                    distlist = distlist + "<option value=" + data[i].distId + ">" + data[i].distName + "</option>";
+                }
+                $("#districtMain").html(distlist);
+            },
+        });
+    });
+    $("#districtMain").change(function () {
+        $.ajax({
+            type: "post",
+            url: "/States/CityList",
+            data: { stateID: $("#stateMain").val(), distID: $("#districtMain").val() },
+            datatype: "json",
+            traditional: true,
+            success: function (data) {
+                // console.log("Selected State Id:" + $('#statelist').val());
+                // console.log(data);
+                var citylist = '<option value="">Select City</option>';
+                for (var i = 0; i < data.length; i++) {
+                    citylist = citylist + "<option value=" + data[i].cityId + ">" + data[i].cityName + "</option>";
+                }
+                $("#cityMain").html(citylist);
+            },
+        });
+    });
+
+    // For loading all Visit Purpose List
+    $.ajax({
+        type: "post",
+        url: "/States/VisitPurposeList",
+        datatype: "json",
+        traditional: true,
+        success: function (data) {
+            console.log(data);
+            vplist = '<option value="">Select Visit Purpose</option>';
+            for (var i = 0; i < data.length; i++) {
+                vplist = vplist + "<option value=" + data[i].id + ">" + data[i].purpose + "</option>";
+            }
+            //district = district + '</select>';
+            $("#visitMain").html(vplist);
+            $("#visitAdult").html(vplist);
+        },
+    });
+    // For loading all ID Proof Type List
+    $.ajax({
+        type: "post",
+        url: "/States/IdProofTypeList",
+        datatype: "json",
+        traditional: true,
+        success: function (data) {
+            console.log(data);
+            idprooflist = '<option value="">Select ID Proof</option>';
+            for (var i = 0; i < data.length; i++) {
+                idprooflist = idprooflist + "<option value=" + data[i].id + ">" + data[i].idProofType + "</option>";
+            }
+            //district = district + '</select>';
+            $("#idtypeMain").html(idprooflist);
+            $("#idTypeAdult").html(idprooflist);
+        },
+    });
+
+   
+
+    $('#addAddOnGuest').click(function () {
+        $("#mainHotelGuest").hide();
+        var name = $("#firstName").val();
+        var addOnGuestCount = parseInt($('#addOnGuestCount').val());
+        var addOnGuestHtml = `
+          <div class="d-flex">
+                    <div class="form-group">
+                        @Html.EditorFor(model => model.addOnGuest[i].firstName, new { htmlAttributes = new { @class = "form-control" ,placeholder = "First Name"} })
+
+                        @Html.ValidationMessageFor(model =>  model.addOnGuest[i].firstName, "", new { @class = "text-danger" })
+                    </div>
+                    <div class="form-group">
+                        @Html.EditorFor(model => model.addOnGuest[i].lastName, new { htmlAttributes = new { @class = "form-control" ,placeholder = "Last Name"} })
+
+                        @Html.ValidationMessageFor(model => model.addOnGuest[i].lastName, "", new { @class = "text-danger" })
+                    </div>
+                    <div class="form-group">
+                        @Html.EditorFor(model => model.addOnGuest[i].mobile, new { htmlAttributes = new { @class = "form-control" ,placeholder = "Mobile"} })
+
+                        @Html.ValidationMessageFor(model => model.addOnGuest[i].mobile, "", new { @class = "text-danger" })
+                    </div>
+                </div>
+                `;
+
+        $('#addOnGuests').append(addOnGuestHtml);
+        addOnGuestCount++;
+        $('#addOnGuestCount').val(addOnGuestCount); // Update the count in the hidden field
+    });
+});
+ function showCard(){
+        alert()
+        $("#mainHotelGuest").hide();
+        $("#finalData").show();
+
+         document.querySelector('#displayFormData').innerHTML += `
+         <div class="card">
+         <div class="d-flex">
+         <div>
+        
+         </div>
+         <div style="margin-left:4%">
+                 <h3><span>Name:</span>${$("#firstName").val() }</h3>
+                    
+                     </div>
+                  </div>
+           </div>
+          `;
+    }
+var video = document.getElementById('video');
+var canvas = document.createElement('canvas');
+var context = canvas.getContext('2d');
+
+navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+    video.srcObject = stream;
+    video.play();
+}).catch(function (err) {
+    console.log(err);
+});
+
+document.getElementById('capture').addEventListener('click', function () {
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    // add time stamp
+    const timeNow = new Date()
+    context.fillStyle = '#000'
+    context.fontSize = '10px'
+    context.fillText(timeNow, 0, canvas.height - 5)
+
+    var image = canvas.toDataURL();
+    //uploadImage(image);
+    //console.log(image)
+    // image placeholder where the image will be displayed
+    var imagecapture = document.getElementById('image-capture');
+    var imagePlaceholder = document.getElementById('image-placeholder');
+
+    // display the image in placeholder
+    displayBase64Image(imagePlaceholder, imagecapture, image);
+});
+
+function displayBase64Image(placeholder, placeholdercapture, base64Image) {
+    var image = document.createElement('img');
+    var image1 = document.createElement('img');
+    image.onload = function () {
+        placeholdercapture.innerHTML = '';
+        placeholdercapture.appendChild(this);
+
+    }
+    image1.onload = function () {
+
+        placeholder.innerHTML = '';
+        placeholder.appendChild(this);
+    }
+    image1.src = base64Image;
+    image.src = base64Image;
+    document.getElementById('guestPhoto').value = base64Image;
+}
+function previewFile() {
+    navigator.getMedia = (navigator.getUserMedia || // use the proper vendor prefix
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+
+    navigator.getMedia({ video: true }, function () {
+        $("#addImage").modal({ backdrop: 'static', keyboard: false }, "show");
+    }, function () {
+        // webcam is not available
+        alert("Web Cam Is Not Available");
+    });
+
+
+
+}
