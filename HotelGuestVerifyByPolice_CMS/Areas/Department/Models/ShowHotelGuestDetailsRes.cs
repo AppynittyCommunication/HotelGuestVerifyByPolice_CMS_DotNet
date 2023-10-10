50814d0d484a3eed377925687c861d812d221386
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2023-10-10 15:05:24 +0530

    Comit By Rohit

---
+++ HotelGuestVerifyByPolice_CMS/Areas/Department/Models/ShowHotelGuestDetailsRes.cs
@@ -0,0 +1,31 @@
+﻿namespace HotelGuestVerifyByPolice_CMS.Areas.Department.Models
+{
+    public class ShowHotelGuestDetailsRes
+    {
+        public int code { get; set; }
+        public string status { get; set; }
+        public string message { get; set; }
+        public List<HotelGuestDetail> hotelGuestDetails { get; set; }
+        public List<AddOnGuestDetails1> addOnGuestDetails1 { get; set; }
+    }
+
+    public class AddOnGuestDetails1
+    {
+        public string relationWithGuest { get; set; }
+        public string guestName { get; set; }
+    }
+
+    public class HotelGuestDetail
+    {
+        public string roomBookingId { get; set; }
+        public string guestName { get; set; }
+        public string email { get; set; }
+        public string mobile { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public object address { get; set; }
+        public string guestPhoto { get; set; }
+    }
+}
