c2e39f85c7f94023f8fafe10cf9ec78b259e35fc
shubhammanohare88@gmail.com <shubhammanohare88@gamil.com>
2023-10-17 13:39:16 +0530

    Commit By Shubham

---
+++ HotelGuestVerifyByPolice_CMS/Areas/HotelDashboard/Models/CommonResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelGuestVerifyByPolice_CMS.Areas.HotelDashboard.Models
+{
+    public class CommonResponse
+    {
+        public int code { get; set; }
+        public string status { get; set; }
+        public string message { get; set; }
+    }
+}
